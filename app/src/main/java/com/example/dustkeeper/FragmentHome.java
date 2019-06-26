4ed875d6f57280640c1815cc06e3484929545a77
Yejin <yeajin357@naver.com>
2019-06-27 01:25:29 +0900

    camera

--- app/src/main/java/com/example/dustkeeper/FragmentHome.java
+++ app/src/main/java/com/example/dustkeeper/FragmentHome.java
@@ -9,6 +9,7 @@ import android.view.View;
 import android.view.ViewGroup;
 
 public class FragmentHome extends Fragment {
+    @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_home, container, false);
     }
