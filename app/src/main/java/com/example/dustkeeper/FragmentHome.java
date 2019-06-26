d40505c818ab281d56ee31d6b1a7e7834e2a3de9
sujin <aoghksj@naver.com>
2019-06-27 01:37:05 +0900

    home_ui

--- app/src/main/java/com/example/dustkeeper/FragmentHome.java
+++ app/src/main/java/com/example/dustkeeper/FragmentHome.java
@@ -4,7 +4,6 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.support.v4.content.ContextCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -23,11 +22,96 @@ public class FragmentHome extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_home, container, false);
 
+        TextView string_location = (TextView) view.findViewById(R.id.string_location);
+
         ImageView image_cloud = (ImageView) view.findViewById(R.id.image_cloud);
         TextView string_state = (TextView) view.findViewById(R.id.string_state);
         TextView dustDegree = (TextView) view.findViewById(R.id.dustDegree);
         TextView ult_dustDegree = (TextView) view.findViewById(R.id.ult_dustDegree);
 
+        /*gps 위치와 비교해서 현재 위치 string 출력*/
+        String gps_location = new String("마포구"); //GPS 받아오기
+
+        switch(gps_location){
+            case "강남구":
+                string_location.setText("서울시 강남구");
+                break;
+            case "강동구":
+                string_location.setText("서울시 강동구");
+                break;
+            case "강서구":
+                string_location.setText("서울시 강서구");
+                break;
+            case "강북구":
+                string_location.setText("서울시 강북구");
+                break;
+            case "관악구":
+                string_location.setText("서울시 관악구");
+                break;
+            case "광진구":
+                string_location.setText("서울시 광진구");
+                break;
+            case "구로구":
+                string_location.setText("서울시 구로구");
+                break;
+            case "금천구":
+                string_location.setText("서울시 금천구");
+                break;
+            case "노원구":
+                string_location.setText("서울시 노원구");
+                break;
+            case "동대문구":
+                string_location.setText("서울시 동대문구");
+                break;
+            case "도봉구":
+                string_location.setText("서울시 도봉구");
+                break;
+            case "동작구":
+                string_location.setText("서울시 동작구");
+                break;
+            case "마포구":
+                string_location.setText("서울시 마포구");
+                break;
+            case "서대문구":
+                string_location.setText("서울시 서대문구");
+                break;
+            case "성동구":
+                string_location.setText("서울시 성동구");
+                break;
+            case "성북구":
+                string_location.setText("서울시 성북구");
+                break;
+            case "서초구":
+                string_location.setText("서울시 서초구");
+                break;
+            case "송파구":
+                string_location.setText("서울시 송파구");
+                break;
+            case "영등포구":
+                string_location.setText("서울시 영등포구");
+                break;
+            case "용산구":
+                string_location.setText("서울시 용산구");
+                break;
+            case "양천구":
+                string_location.setText("서울시 양천구");
+                break;
+            case "은평구":
+                string_location.setText("서울시 은평구");
+                break;
+            case "종로구":
+                string_location.setText("서울시 종로구");
+                break;
+            case "중구":
+                string_location.setText("서울시 중구");
+                break;
+            case "중랑구":
+                string_location.setText("서울시 중랑구");
+                break;
+
+        }
+
+        /*농도 받아와서 상태, 농도 출력*/
         int degree = 36; //DB에서 받아오기
         int ult_degree = 30;//DB에서 받아오기
 
